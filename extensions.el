29a29aafcd891f7312653a322e450aba88be0d21
Olivier Verdier <olivier.verdier@gmail.com>
2016-01-26 11:25:53 +0100

    basic layer for coq and proof-general
    directly based on
    https://gist.github.com/puffnfresh/dc9842076d261c95f798

---
+++ extensions.el
@@ -0,0 +1,43 @@
+;;; extensions.el --- coq Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq coq-pre-extensions
+      '(
+        proof-general
+        ))
+
+(setq coq-post-extensions
+      '(
+        ;; post extension names go here
+        ))
+
+;; For each extension, define a function coq/init-<extension-name>
+;;
+;; (defun coq/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+
+(defun coq/init-proof-general ()
+  "Initialize Proof General"
+  (use-package proof-site
+    :defer t
+    :mode ("\\.v\\'" . coq-mode)
+    :load-path
+    "/usr/local/Cellar/coq/4.2/share/emacs/site-lisp/coq/generic")
+  :config (progn
+            (evil-leader/set-key-for-mode 'coq-mode
+              "mn" 'proof-assert-next-command-interactive))
+  )
